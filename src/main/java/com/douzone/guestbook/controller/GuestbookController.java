b242c2defa3978e104b86e8c1fc9c294e32259c9
BIT <BIT@DESKTOP-E23PFRN>
2019-02-18 16:14:34 +0900

    190218 16:14 Spring-guestbook

---
+++ src/main/java/com/douzone/guestbook/controller/GuestbookController.java
@@ -0,0 +1,45 @@
+package com.douzone.guestbook.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.douzone.guestbook.dao.GuestbookDao;
+import com.douzone.guestbook.vo.GuestbookVo;
+
+@Controller
+public class GuestbookController {
+	
+	@Autowired
+	private GuestbookDao guestbookDao;
+	
+	@RequestMapping("")
+	public String index(Model model) {
+		model.addAttribute("list", guestbookDao.getList());
+		return "/WEB-INF/views/index.jsp";
+	}
+	
+	@RequestMapping("deleteform")
+	public String deleteform(
+			@RequestParam(value="no", required=true) Long no) {
+		return "/WEB-INF/views/deleteform.jsp";
+	}
+	
+	@RequestMapping(value="/delete", method=RequestMethod.POST)
+	public String delete(GuestbookVo guestbookVo) {
+		guestbookDao.delete(guestbookVo);
+		return "redirect:/";
+		
+	}
+	
+	@RequestMapping(value="/add", method=RequestMethod.POST)
+	public String add(GuestbookVo guestbookVo) {
+		guestbookDao.insert(guestbookVo);
+		return "redirect:/";
+	}
+
+}
